77b37d205338f875617b7fd34893d6aafd1b968e
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-11-08 17:56:56 +0000

    Deploying to gh-pages from @ julika-gulchitai/goit-js-hw-08@121f402b49792a546d3d3c509115a3072faa6d0e 🚀

---
+++ 02-video.978171ae.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("play",(function(){console.log("played the video!")})),n.getVideoTitle().then((function(e){console.log("title:",e)})),timeupdate;n.on("play",(function(e){})),n.setCurrentTime(30.456).then((function(e){})).catch((function(e){e.name}))}();
+//# sourceMappingURL=02-video.978171ae.js.map
