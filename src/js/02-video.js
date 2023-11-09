2fed9171e41c553494b4ffb4b0b8ae42bea9e16d
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-11-10 01:55:13 +0200

    8-2 task fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const stopTime = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p'
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(stopTime, JSON.stringify(seconds));
+};
+// timeupdate - оновлення часу відтворення.
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(stopTime))).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+/* 
+Зберігай час відтворення у локальне сховище. 
+Нехай ключем для сховища буде рядок "videoplayer-current-time".
+Під час перезавантаження сторінки скористайся методом setCurrentTime() 
+з метою відновлення відтворення зі збереженої позиції.
+Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.*/
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+/*
+Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. 
+Нехай ключем для сховища буде рядок "feedback-form-state".
+Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.*/
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('input');
+
+const STORAGE = "feedback-form-state";
+let contactForm =  {};
+
+
+function fillContactForm(event) {
+console.log('event', event);
+    for (const key in contactForm) {
+        if (contactForm.hasOwnProperty(key)) {
+            contactForm.elements[key].value = event[key];
+        }
+    }
+}
+//   if (userForm === undefined) {
+//     return;
+//   }
+
+  console.log(contactForm);
+
+  // contactFormElements.user_name.value = userDataFromLS.user_name;
+  // contactFormElements.user_email.value = userDataFromLS.user_email;
+  // contactFormElements.user_message.value = userDataFromLS.user_message;
+form.addEventListener('input', throttle( fillContactForm, 500));
+// form.addEventListener('submit', e => {
+    
+// })
+
+const onContactFormFieldChange = ({ target: contactFormField }) => {
+  const contactFormFieldValue = contactFormField.value;
+  const contactFormFieldName = contactFormField.name;
+
+  userData[contactFormFieldName] = contactFormFieldValue;
+
+  localStorageAPI.save('contactFormInfo', userData);
+};
+
+const onContactFormSubmit = event => {
+  event.preventDefault();
+
+  contactFormEl.reset();
+  localStorageAPI.remove('contactFormInfo');
+};
+
+contactFormEl.addEventListener('change', onContactFormFieldChange);
+contactFormEl.addEventListener('submit', onContactFormSubmit);
+
+
+localStorage.removeItem()
+event.currentTargettarget.reset();
